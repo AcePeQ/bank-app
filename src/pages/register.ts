14d1ec54ec50b9e174de54f5eb0b346a316b3ca1
AcePeQ <maciej.nojszewski22@gmail.com>
2026-07-17 11:50:19 +0200

    feat: create first validation and register functions

--- src/pages/register.ts
+++ src/pages/register.ts
@@ -1,11 +1,59 @@
 import { createIcons, Eye } from "lucide";
 
 function init() {
-  createIcons({
+    createIcons({
     icons: {
       Eye,
     }
   })
+
+  const form = document.querySelector("#form") as HTMLFormElement;
+  const firstNameInputEl = document.querySelector("#firstName") as HTMLInputElement;
+  const lastNameInputEl = document.querySelector("#lastName") as HTMLInputElement;
+  const emailInputEl = document.querySelector("#email") as HTMLInputElement;
+  const phoneNumberInputEl = document.querySelector("#phoneNumber") as HTMLInputElement;
+  const passwordInputEl = document.querySelector("#password") as HTMLInputElement;
+  const confirmPasswordInputEl = document.querySelector("#confirmPassword") as HTMLInputElement;
+  const termsCheckboxEl = document.querySelector("#terms") as HTMLInputElement
+
+
+  function handleValidationInput(inputEl:HTMLInputElement, errorEl: HTMLParagraphElement) {
+    const name = inputEl.getAttribute("name");
+
+    switch (name) {
+      case "firstName":
+        break;
+      case "lastName":
+        break;
+      case "email":
+        break;
+      case "phoneNumber":
+        break;
+      case "password":
+        break;
+      case "confirmPassword":
+        break;
+        default:
+    }
+  }
+
+  function handleInput(event: InputEvent) {
+    const target = event.target as HTMLInputElement;
+    if(!target) return;
+
+    const inputRowEl = target.closest(".form__row") as HTMLDivElement
+    if(!inputRowEl) return;
+
+    const inputEl = inputRowEl.querySelector(".form__input") as HTMLInputElement;
+    const errorEl = inputRowEl.querySelector(".form__error") as HTMLParagraphElement;
+    
+    handleValidationInput(inputEl, errorEl)
+  }
+
+
+
+  // form.addEventListener("submit", handleSubmit);
+  form.addEventListener("input", handleInput);
 };
 
 init();
